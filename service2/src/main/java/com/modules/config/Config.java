cb4fda6ee4f370cad92fb59a30dcd054281e273f
vector <vector.wang@hirede.com>
2017-04-01 10:32:20 +0800

    springboot 多模块

---
+++ service2/src/main/java/com/modules/config/Config.java
@@ -0,0 +1,15 @@
+package com.modules.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2017/3/31
+ * Time: 13:58
+ */
+@Configuration
+@ImportResource("classpath:spring-*")
+public class Config {
+}
